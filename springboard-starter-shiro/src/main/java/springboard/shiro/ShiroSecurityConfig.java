dec2f85085830fa406b3cb5de4ec2d864baeb333
Damaged`Boy <damagedboycn@gmail.com>
2018-10-09 15:34:52 +0800

    解决shiro @RequiresRole注解造成mapping无法映射的问题

--- springboard-starter-shiro/src/main/java/springboard/shiro/ShiroSecurityConfig.java
+++ springboard-starter-shiro/src/main/java/springboard/shiro/ShiroSecurityConfig.java
@@ -8,6 +8,7 @@ import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +38,19 @@ public class ShiroSecurityConfig implements WebMvcConfigurer {
         DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
         defaultWebSecurityManager.setRealm(realm);
         return defaultWebSecurityManager;
+    }
+
+    @ConditionalOnMissingBean
+    @Bean
+    public DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
+        /**
+         * setUsePrefix(false)用于解决一个奇怪的bug。在引入spring aop的情况下。
+         * 在@Controller注解的类的方法中加入@RequiresRole注解，会导致该方法无法映射请求，导致返回404。
+         * 加入这项配置能解决这个bug
+         */
+        creator.setUsePrefix(true);
+        return creator;
     }
 
     @ConditionalOnMissingBean
