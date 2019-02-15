2d845425c0617f83bea1d706c5ac4b168e672a2d
ching <Sdv123123>
2019-02-15 15:31:12 +0800

    试了一下@Transactional，可以的。

--- springboard-example/springboard-example-service/src/test/java/springboard/example/service/impl/TestAdminServiceImpl.java
+++ springboard-example/springboard-example-service/src/test/java/springboard/example/service/impl/TestAdminServiceImpl.java
@@ -1,17 +1,21 @@
 package springboard.example.service.impl;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 import springboard.example.model.AdminService;
 import springboard.example.model.Role;
 import springboard.example.model.User;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -110,6 +114,24 @@ public class TestAdminServiceImpl {
     @Test
     public void testSetRolePermissions() {
         adminService.setRolePermissions(4,  "troopers:read", "troopers:create", "troopers:update", "troopers:delete");
+    }
+
+    @Transactional
+    public boolean someTransaction() {
+        User user = adminService.getUser(3);
+        user.setLastLoggedInTime(new Date());
+        user.setLastLoggedInAddr(null);
+        adminService.updateUser(user);
+        if(RandomUtils.nextInt() % 2 == 1) throw new RuntimeException("odd fate");
+        user.setLastLoggedInAddr("127.0.0.1");
+        adminService.updateUser(user);
+        return true;
+    }
+
+    @Test
+    public void testSomeTransaction() throws InterruptedException {
+        someTransaction();
+        Thread.sleep(100L);
     }
 
 }
