2f9a2b5e5b0b9aff59aa97bfdf12f5e77ea991cc
chings <chings@foxmail.com>
2021-08-25 15:32:26 +0800

    resolve the problem in DefaultXMLLanguageDriver, absolutely!

--- springboard-starter-mybatis/src/main/java/springboard/mybatis/component/DefaultXMLLanguageDriver.java
+++ springboard-starter-mybatis/src/main/java/springboard/mybatis/component/DefaultXMLLanguageDriver.java
@@ -7,14 +7,23 @@ import org.apache.ibatis.session.Configuration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * supported grammar:
+ *  field in #{values}
+ *  field IN (#{values})
+ *  field In ( #{ values } )
+ */
+
 public class DefaultXMLLanguageDriver extends MybatisXMLLanguageDriver {
 
-    private static final Pattern collectionPattern = Pattern.compile("([Ii][Nn]\\s+)(\\(?#\\{\\s*)(\\w+)(\\s*\\}\\)?)");
+    private static final Pattern collectionPattern = Pattern.compile("([Ii][Nn]\\s+)(\\(?)(\\s*#\\{\\s*)(\\w+)(\\s*\\}\\s*)(\\)?)");
 
     public SqlSource createSqlSource(Configuration configuration, String script, Class<?> parameterType) {
         Matcher matcher = collectionPattern.matcher(script);
         if (matcher.find()) {
-            script = matcher.replaceAll("$1<foreach collection=\"$3\" item=\"$3_item\" open=\"(\" separator=\",\" close=\")\" >#{$3_item}</foreach>");
+            StringBuilder replacement = new StringBuilder("$1<foreach collection=\"$4\" item=\"$4_item\" open=\"(\" separator=\",\" close=\")\" >#{$4_item}</foreach>");
+            if(matcher.group(2).isEmpty()) replacement.append(matcher.group(6));
+            script = matcher.replaceAll(replacement.toString());
         }
 
         if(!script.startsWith("<script>")) script = "<script>" + script;
